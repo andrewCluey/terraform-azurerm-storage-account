efe9dc94e4d5b5e8ae32c3522bafff1d75bbb805
ASC IT Solutions <andrew@asc-solutions.co.uk>
2020-11-04 12:08:12 +0000

    added new output values

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "storage_account_name" {
 output "private_endpoint_ip_address" {
   value = azurerm_private_endpoint.pe.private_service_connection[0].private_ip_address
 }
+
+output "primary_blob_endpoint" {
+  value = azurerm_storage_account.pe_storage_account.primary_blob_endpoint
+}
+
+output "primary_connection_string" {
+  value = azurerm_storage_account.pe_storage_account.primary_connection_string
+}

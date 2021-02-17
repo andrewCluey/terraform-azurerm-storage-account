18ab13d681c7565ab2fc7da3c9186006be4653ed
ASC IT Solutions <andrew@asc-solutions.co.uk>
2021-02-17 11:00:03 +0000

    added new outputs

--- outputs.tf
+++ outputs.tf
@@ -2,14 +2,19 @@ output "storage_account_name" {
   value = azurerm_storage_account.pe_storage_account.name
 }
 
-#output "private_endpoint_ip_address" {
-#  value = azurerm_private_endpoint.pe_blob.private_service_connection.private_ip_address
-#}
-
 output "primary_blob_endpoint" {
   value = azurerm_storage_account.pe_storage_account.primary_blob_endpoint
 }
 
 output "id" {
   value = azurerm_storage_account.pe_storage_account.id
+}
+
+output "private_endpoint_ip_address" {
+  value = azurerm_private_endpoint.pe_blob.private_service_connection.private_ip_address
+}
+
+output "default_blob_endpoint" {
+  description = "The endpoint URL for the default blob storage account in the primary location"
+  value       = azurerm_storage_account.pe_storage_account.primary_blob_endpoint
 }
